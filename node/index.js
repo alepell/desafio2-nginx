12ac0122be0e7d505aedc99412dfd2b29def4fb6
Alexandre Freire Pellegrino <alesirius@gmail.com>
2021-02-26 18:07:24 -0300

    first commit

---
+++ node/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = 'INSERT INTO pessoas(name) values ("Alexandre")'
+
+connection.query(sql)
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/', function (req, res) {
+    var sql = 'SELECT name FROM pessoas'
+    connection.query(sql, function (err, data, fields) {
+        if (err) throw err
+        res.render('index.ejs', { pessoas: data })
+        connection.end
+    });
+});
+
+app.listen(port, () => {
+    console.log('Rodando na porta ' + port)
+})
